0c9f0a1fe41570eeb9167338a9698904afbbbf59
Richard Davis <rddavistx@hotmail.com>
2016-10-22 16:59:39 -0500

    no db

--- bootstrap.php
+++ bootstrap.php
@@ -18,8 +18,17 @@ function format_print_r($output) {
 // echo 'ENV database:' . $_ENV['DATABASE_NAME'];
 
 
-$dbh = new PDO('mysql:host=' . $_ENV['DATABASE_HOST'] . ';dbname=' . $_ENV['DATABASE_NAME'], $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
-
-
+// $dbh = new PDO('mysql:host=' . $_ENV['DATABASE_HOST'] . ';dbname=' . $_ENV['DATABASE_NAME'], $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
+//
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db = substr($cleardb_url["path"], 1);
+//
+// $db['default']['hostname'] = $cleardb_server;
+// $db['default']['username'] = $cleardb_username;
+// $db['default']['password'] = $cleardb_password;
+// $db['default']['database'] = $cleardb_db;
 
  ?>
