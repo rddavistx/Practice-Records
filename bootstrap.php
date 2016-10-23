91f22fe3248d2a4c3ee8b593e60b39a0a2cf6741
Richard Davis <rddavistx@hotmail.com>
2016-10-23 09:31:53 -0500

    db connect

--- bootstrap.php
+++ bootstrap.php
@@ -18,6 +18,21 @@ function format_print_r($output) {
 // echo 'ENV database:' . $_ENV['DATABASE_NAME'];
 
 
+
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+// $conn = new mysqli($server, $username, $password, $db);
+
+
+
+
+
 $dbh = new PDO('mysql:host=' . $_ENV['DATABASE_HOST'] . ';dbname=' . $_ENV['DATABASE_NAME'], $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
 //
 // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
