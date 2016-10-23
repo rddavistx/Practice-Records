64b967653a4318341b2c687a58ddd8a39b0cdbd5
Richard Davis <rddavistx@hotmail.com>
2016-10-23 09:53:04 -0500

    conn to dbh

--- bootstrap.php
+++ bootstrap.php
@@ -26,7 +26,7 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-$conn = new mysqli($server, $username, $password, $db);
+$dbh = new mysqli($server, $username, $password, $db);
 
 
 
